5df389a2bc76a22250b5ee71bfbec7b5cb633bad
Ovidio R. Calvet <calveto@gmail.com>
2019-03-14 19:38:12 -0400

    Add more endpoints

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ app.get('/', (req, res) => {
     res.json({ hi: 'Master of none!'});
 });
 
+app.get('/public', (req, res) => {
+    res.json({ visible: 'Everyone can see this line' });
+});
+
+app.get('/private', (req, res) => {
+    res.json({ visible: 'Only you can see it' });
+});
+
+app.get('/private', (req, res) => {
+    res.json({ visible: 'Only you can see it' });
+});
+
 app.listen(3080, () => {
     console.log('Listening on 3080');
 });
