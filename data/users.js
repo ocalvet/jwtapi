d81f53cf1e298bd4d74c1856b7489fd699169ccf
Ovidio R. Calvet <calveto@gmail.com>
2019-03-14 20:21:38 -0400

    Add sample user data

---
+++ data/users.js
@@ -0,0 +1,29 @@
+module.exports = [
+  {
+    _id: "1",
+    name: "Test0 Lastname",
+    email: "test0@email.com",
+    password: "pass123",
+    roles: ["admin"]
+  },
+  {
+    _id: "2",
+    name: "Test1 Lastname",
+    email: "test1@email.com",
+    password: "pass123",
+    roles: ["author"]
+  },
+  {
+    _id: "3",
+    name: "Test2 Lastname",
+    email: "test2@email.com",
+    password: "pass123",
+    roles: ["reader"]
+  },
+  {
+    _id: "4",
+    name: "Test3 Lastname",
+    email: "test3@email.com",
+    password: "pass123"
+  }
+];
