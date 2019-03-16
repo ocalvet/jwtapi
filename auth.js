a8c2247a8464ae161be2fb89826b52064c334c85
Ovidio R. Calvet <calveto@gmail.com>
2019-03-16 18:07:56 -0400

    Extract getting the token into a function

--- auth.js
+++ auth.js
@@ -1,30 +1,37 @@
-const jwt = require("jsonwebtoken");
-const config = require("./config.js");
+const jwt = require('jsonwebtoken');
+const config = require('./config.js');
 
-const checkToken = (req, res, next) => {
-  let token = req.headers["x-access-token"] || req.headers["authorization"]; // Express headers are auto converted to lowercase
-  if (token && token.startsWith("Bearer ")) {
+const getTokenFromRequest = req => {
+  let token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
+  if (token && token.startsWith('Bearer ')) {
     token = token.slice(7, token.length);
   } else {
+    throw new Error('Token not found');
+  }
+  return token;
+};
+
+const checkToken = (req, res, next) => {
+  try {
+    const token = this.getTokenFromRequest(req);
+    jwt.verify(token, config.secret, (err, decoded) => {
+      if (err) {
+        res.status(401);
+        return res.end();
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  } catch (e) {
     res.status(401);
     return res.end();
   }
-  jwt.verify(token, config.secret, (err, decoded) => {
-    if (err) {
-      res.status(401);
-      return res.end();
-    } else {
-      console.log("Decoded token");
-      console.log(decoded);
-      req.decoded = decoded;
-      next();
-    }
-  });
 };
 
 const sign = data => {
   let token = jwt.sign(data, config.secret, {
-    expiresIn: "24h" // expires in 24 hours
+    expiresIn: '24h' // expires in 24 hours
   });
   return token;
 };
